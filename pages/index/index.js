525f9e2308e685f2db3071adcdd4ab35a2707aa7
forska <736523388@qq.com>
2021-09-11 13:52:51 +0800

    商城使用colorUI

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,23 +1,43 @@
+const createRecycleContext = require('miniprogram-recycle-view')
 Page({
   data: {
-    active: 'index',
+    PageCur: 'basics'
   },
-  onChange(event) {
-    if(event.detail == this.data.active){
-      return false
-    }
-    let router = `/pages/${event.detail.split('_').join('/')}/index`
-    wx.navigateTo({
-      url: router,
-      success: function (res) {
-        // 通过eventChannel向被打开页面传送数据
-        res.eventChannel.emit('acceptDataFromOpenerPage', {
-          data: 'test'
-        })
-      },
-      fail: ref => {
-        console.log(ref)
+  onReady: function () {
+    var ctx = createRecycleContext({
+      id: 'recycleId',
+      dataKey: 'recycleList',
+      page: this,
+      itemSize: { // 这个参数也可以直接传下面定义的this.itemSizeFunc函数
+        width: 162,
+        height: 182
       }
     })
+    ctx.append([
+      {
+        image_url: 'http://image.cqclxsc.com/goods/36/16/1.png',
+        idx: 0,
+        title: '测试'
+      },
+      {
+        image_url: 'http://image.cqclxsc.com/goods/36/16/1.png',
+        idx: 0,
+        title: '测试'
+      }
+    ])
+    // ctx.update(beginIndex, list)
+    // ctx.destroy()
   },
-});
+  NavChange(e) {
+    this.setData({
+      PageCur: e.currentTarget.dataset.cur
+    })
+  },
+  onShareAppMessage() {
+    return {
+      title: 'ColorUI-高颜值的小程序UI组件库',
+      imageUrl: '/images/share.jpg',
+      path: '/pages/index/index'
+    }
+  },
+})
