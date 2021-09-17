621248a83598f0ee9329eb5cf1735c0c388131b2
forska <736523388@qq.com>
2021-09-17 16:46:35 +0800

    图标替换

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,27 +4,27 @@ Page({
     PageCur: 'basics'
   },
   onReady: function () {
-    var ctx = createRecycleContext({
-      id: 'recycleId',
-      dataKey: 'recycleList',
-      page: this,
-      itemSize: { // 这个参数也可以直接传下面定义的this.itemSizeFunc函数
-        width: 162,
-        height: 182
-      }
-    })
-    ctx.append([
-      {
-        image_url: 'http://image.cqclxsc.com/goods/36/16/1.png',
-        idx: 0,
-        title: '测试'
-      },
-      {
-        image_url: 'http://image.cqclxsc.com/goods/36/16/1.png',
-        idx: 0,
-        title: '测试'
-      }
-    ])
+    // var ctx = createRecycleContext({
+    //   id: 'recycleId',
+    //   dataKey: 'recycleList',
+    //   page: this,
+    //   itemSize: { // 这个参数也可以直接传下面定义的this.itemSizeFunc函数
+    //     width: 162,
+    //     height: 182
+    //   }
+    // })
+    // ctx.append([
+    //   {
+    //     image_url: 'http://image.cqclxsc.com/goods/36/16/1.png',
+    //     idx: 0,
+    //     title: '测试'
+    //   },
+    //   {
+    //     image_url: 'http://image.cqclxsc.com/goods/36/16/1.png',
+    //     idx: 0,
+    //     title: '测试'
+    //   }
+    // ])
     // ctx.update(beginIndex, list)
     // ctx.destroy()
   },
